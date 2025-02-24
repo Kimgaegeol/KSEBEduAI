e1abda66f5a89f7a508038cb6e873744c35b40a2
Kimgaegeol <flying0305@naver.com>
2025-02-24 19:20:59 +0900

    v1.5

--- day01_2.py
+++ day01_2.py
@@ -3,24 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
-from tglearn import LinearRegression as tg
+from tglearn import MyLinearRegression, MyKNeighborsRegressor
 
 ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
 x = ls[["GDP per capita (USD)"]].values
+print(x)
 y = ls[["Life satisfaction"]].values
+print(y)
 
 # ls.plot(kind="scatter", grid=True, x="GDP per capita (USD)", y="Life satisfaction")
 # plt.axis([23500,62500,4,9])
 # plt.show()
+#
+# model = LinearRegression()
+# model2 = KNeighborsRegressor(n_neighbors=3)
+# model3 = MyLinearRegression()
+# model.fit(x,y)
+# model2.fit(x,y)
+# model3.fit(x,y)
+#
+# x_new = [[31721.3]] #ROK 2020
+# print(model.predict(x_new))
+# print(model2.predict(x_new))
+# print(model3.predict(x_new))
 
-model = LinearRegression()
-model2 = KNeighborsRegressor(n_neighbors=3)
-model3 = tg()
+model = MyKNeighborsRegressor()
 model.fit(x,y)
-model2.fit(x,y)
-model3.fit(x,y)
-
-x_new = [[31721.3]] #ROK 2020
-print(model.predict(x_new))
-print(model2.predict(x_new))
-print(model3.predict(x_new))
+model.predict(30000)
--- tglearn.py
+++ tglearn.py
@@ -1,6 +1,52 @@
 import numpy as np
 
-class LinearRegression:
+def quick_sort(l):
+    n = len(l)
+    if n <= 1: return l
+
+    # x_l에 (value[0], index) 튜플을 저장
+    x_l = [(value[0], i) for i, value in enumerate(l)]
+
+    pivot = x_l[n // 2][0]  # pivot 값 설정
+
+    left, mid, right = [], [], []
+
+    # value, index로 언패킹 가능
+    for value, index in x_l:
+        if value < pivot:
+            left.append(l[index])
+        elif value > pivot:
+            right.append(l[index])
+        else:
+            mid.append(l[index])
+
+    return quick_sort(left) + mid + quick_sort(right)
+
+    return quick_sort(left)+mid+quick_sort(right)
+
+class MyKNeighborsRegressor:
+    def __init__(self, n_neighbors = 5):
+        self.n_neighbors = n_neighbors
+        self.list = []
+
+    def fit(self,x,y):
+        for _ in range(len(x)):
+            self.list.append([float(x[_][0]),float(y[_][0])])
+        self.list = quick_sort(self.list)
+
+    def predict(self, new_x):
+        self.list.append([new_x,None])
+        self.list = quick_sort(self.list)
+        head = 0
+        for i in range(len(self.list)):
+            if self.list[i][0] == new_x:
+                head = i
+                break
+        l = []
+
+
+
+class MyLinearRegression:
     def __init__(self):
         self.slope = None
         self.intercept = None
